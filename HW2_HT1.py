11eff6830b309e9c71915c37afd5661ce246bd9a
Yauheni_C <chescraven@gmail.com>
2023-04-12 19:41:39 +0300

    initial commit create file

---
+++ HW2_HT1.py
@@ -0,0 +1,8 @@
+# Пользователь вводит предложение, заменить все пробелы на "-" 2-мя способами
+# 1
+new_text = input('Enter text: ')
+print(new_text)
+
+# 2
+new_text = input('Enter text: ')
+print(new_text)
