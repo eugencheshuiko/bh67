b29aea9bf2815814eea20047ee8cfaa2531b056b
Yauheni_C <chescraven@gmail.com>
2023-04-12 19:50:37 +0300

    first changes

--- HW2_HT1.py
+++ HW2_HT1.py
@@ -1,8 +1,10 @@
 # Пользователь вводит предложение, заменить все пробелы на "-" 2-мя способами
 # 1
 new_text = input('Enter text: ')
+
 print(new_text)
 
 # 2
 new_text = input('Enter text: ')
+
 print(new_text)
